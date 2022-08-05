75f402733132e733b0dd1db3767910738de78e73
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-08-05 04:27:06 +0300

    end

--- src/test/java/org/Lesson6/ProductTest.java
+++ src/test/java/org/Lesson6/ProductTest.java
@@ -34,7 +34,6 @@ public class ProductTest {
 
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         sqlSession = sqlSessionFactory.openSession();
-        categoriesMapper = sqlSession.getMapper(CategoriesMapper.class);
         productsMapper = sqlSession.getMapper((ProductsMapper.class));
     }
 
